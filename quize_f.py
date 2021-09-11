da2e02f6553dbf6f81469ee20199e11b71dc1c23
abdullahi-7 <abdullahi.abdi@protonmail.com>
2021-09-11 12:59:07 +0300

    print is show something you write

--- quize_f.py
+++ quize_f.py
@@ -1,26 +1,30 @@
 #abdullahi, 10, kenya
 
 def quize():
-    v = input("Do you want to play? ")
-    a = "yes"
 
-    if v == a:
-        print("les tart to play!!!!!!.")
-    else:
-        print("you are done!!!!")
-        quit()
     va = int(input("how many planets are in the sola sestem? "))
     an = 8
-
     if va == an:
         print("correct!!!: )")
     else:
         print("incorrect") 
 
-    var = input("do you want milk? ")
+    var = input("do you want milk? ").lower()
     ans = "yes"
 
     if var == ans:
         print("correct!!! ")
     else:
         print("incorrect")
+    variable = input("it is rainny? ")
+    avswer = False
+
+    if variable == avswer:
+        print("correct!!!")
+    else:
+        print("incorrect!!")
+
+
+
+
+quize()
